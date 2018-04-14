a70d61271abf159259ec55ed492de43a23af38e9
vinayBomma <vinaysb2000@gmail.com>
2018-04-14 06:54:21 +0530

    Made major changes. Remove Modal and added input form in card itself. Implemented Mobile first pattern.
    Took 2 hours 26 minutes

--- js/app.js
+++ js/app.js
@@ -7,4 +7,8 @@ $(document).ready(function(){
     $('.fixed-action-btn').floatingActionButton();
 
     $('.modal').modal();
+
+    $('.datepicker').datepicker();
+
+    $('.timepicker').timepicker();
 });
