f813716e14dd2b1ccd555d13170d45e1a529ae80
vinayBomma <vinaysb2000@gmail.com>
2018-04-17 05:24:46 +0530

    Fixed Preview Bug in Modal. Created init.js for initializing Materialize. Many changes were made.
    Took 7 hours 34 minutes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
-// var elem = document.querySelector('.sidenav');
-// var instance = M.Sidenav.init(elem, options);
+const init = new INIT();
+init.startInit();
 
-$(document).ready(function(){
-    $('.sidenav').sidenav();
 
-    $('.fixed-action-btn').floatingActionButton();
+const eventName = document.getElementById('event_name');
+const datePick = document.getElementById('date_pick');
+const timePick = document.getElementById('time_pick');
+const cardEvent = document.querySelector('.card_event_name');
 
-    $('.modal').modal();
 
-    $('.datepicker').datepicker();
-
-    $('.timepicker').timepicker();
+eventName.addEventListener('keyup', function () {
+    cardEvent.textContent = eventName.value;
 });
+
+// something.appendChild(document.createTextNode(datePick.value));
+
+
