5aac734633df738b1588bfda67b07a6726cd3d0d
vinayBomma <vinaysb2000@gmail.com>
2018-09-27 20:12:05 +0530

    Added PWA functionality

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,24 @@
-// importScripts('/cache-polyfill.js');
-// self.addEventListener('install', function (e) {
-//     e.waitUntil(
-//         caches.open('since').then(function (cache) {
-//             return cache.addAll([
-//                 '/',
-//                 '/?homescreen=1',
-//                 'public/js/app.js',
-//                 'public/js/init.js',
-//                 'public/css/main.css',
-//             ])
-//         })
-//     )
-// });
-//
-// self.addEventListener('fetch', function (event) {
-//     event.respondWith(
-//         caches.match(event.request).then(function (response) {
-//             return response || fetch(event.request);
-//         })
-//     );
-// });
-//
+importScripts('/cache-polyfill.js');
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('since').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/?homescreen=1',
+                '/public/js/app.js',
+                '/public/js/init.js',
+                '/server.js',
+                '/public/css/main.css',
+                '/index.html'
+            ])
+        })
+    )
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
