e3087d24a07d71625bd4339c7e5e3be4db854863
vinayBomma <vinaysb2000@gmail.com>
2018-11-17 08:31:13 +0530

    New API keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-    mongoURI: "mongodb://vinayBomma:CzC981ipjnp0@ds233320.mlab.com:33320/since",
+    mongoURI: "mongodb://bomma_vinay:(vinay)150@ds233320.mlab.com:33320/since",
     googleClientID: "309012755454-ulbpvhhinatevqjtpe181i1q7hf0hpa0.apps.googleusercontent.com",
-    googleClientSecret: "NLx_GSte6A050jXF2Ogy6Rsy",
-    twitterClientID: "2xdK0pSoF4Wvg3cN1pHD9a0Bn",
-    twitterClientSecret: "EoMiIdJL71jf9bbl63ac0eFlzLoknkM2TAG4GbRHNo6337fE8w",
+    googleClientSecret: "MoPx8AZaLRuxz70JBgl4vorH",
+    twitterClientID: "wAzxzToiaBmE3aGSr07iXEqwd",
+    twitterClientSecret: "0sriz0acd4syll3f8pVp3Z9w6rsS2Gws0VajENSQ3vBAGODN5T",
 };
