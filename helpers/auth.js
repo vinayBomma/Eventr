efa5f2ae4b14ba499b4f9c8e8a2d5ae33a0ee8a3
vinayBomma <vinaysb2000@gmail.com>
2018-11-26 07:47:13 +0530

    Delete Confirmation

--- helpers/auth.js
+++ helpers/auth.js
@@ -4,7 +4,7 @@ module.exports = {
             return next();
         }
 
-        req.flash('error_msg', 'Not Authorized');
+        req.flash('error_msg', 'Not Authorized. Please Login');
         res.redirect('/login');
     }
 };
